1ecfa154490bfdeb1a86f2bb9824fd36d042676d
hasan <hasankanj2000@gmail.com>
2022-12-10 20:24:37 +0300

    Basic Queries

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -1,10 +1,10 @@
-1. select Name from students 
-2. select * from students where Age > 30
-3. select name from students where Gender ='F' AND Age >=30
-4. select Points FROM students WHERE name ='Alex'
-5. INSERT INTO students (ID,name,Age,Gender,Points) VALUES (7,'Kanj',22,'M',400);
-6. UPDATE students SET Points = 350 WHERE name = 'Basma';
-7. UPDATE students SET Points = 150 WHERE name = 'Alex';
-
+    1. select Name from students 
+    2. select * from students where Age > 30
+    3. select name from students where Gender ='F' AND Age >=30
+    4. select Points FROM students WHERE name ='Alex'
+    5. INSERT INTO students (ID,name,Age,Gender,Points) VALUES (7,'Kanj',22,'M',400);
+    6. UPDATE students SET Points = 350 WHERE name = 'Basma';
+    7. UPDATE students SET Points = 150 WHERE name = 'Alex';
+    
 
 
