ae9213adf7e85a41458ac1d12d4eabb83396ddb4
hasan <hasankanj2000@gmail.com>
2022-12-15 10:59:56 +0300

    final answers

--- 03_Exercises/04_Database/sql-exercise-master/answers.sh
+++ 03_Exercises/04_Database/sql-exercise-master/answers.sh
@@ -13,7 +13,32 @@
        Gender text,
        Points int,
        Graduation text  );
+   
+     ### Creating Table
 
-    10  INSERT INTO graduates (ID,name,Age,Gender,Points) VALUES (4,'LAyal',18,'F',350);
-    11   UPDATE graduates set Graduation = '08/09/2018' WHERE name = 'Layal'
-    12   delete from students where id= '4'
+     
+    10.  INSERT INTO graduates (ID,name,Age,Gender,Points) VALUES (4,'LAyal',18,'F',350);
+    11.  UPDATE graduates set Graduation = '08/09/2018' WHERE name = 'Layal'
+    12.   delete from students where id= '4'
+
+   13.### Joins
+
+    14.SELECT employees.Name, employees.Company, companies.Date FROM employees 
+       INNER JOIN companies ON companies.Name = employees.Company
+    
+    15.SELECT employees.Name FROM employees 
+       INNER JOIN companies ON companies.Name = employees.Company
+       WHERE companies.Date < 2000
+
+    16.SELECT companies.Name FROM employees 
+       INNER JOIN companies ON companies.Name = employees.Company
+       WHERE employees.Role = 'Graphic Designer'
+
+    17.### Count & Filter
+
+
+      18-SELECT id,name,age,gender, MAX(Points) FROM students
+      19-SELECT  AVG(Points)  FROM students
+      20-SELECT  COUNT(name)  FROM students where Points=500
+      21-SELECT name FROM students WHERE name LIKE '%s%';
+      22-SELECT * FROM students ORDER BY Points DESC;
