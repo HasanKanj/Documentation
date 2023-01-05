7a13b6c92c3280d16d380d21e91ef172228e9153
hasan <hasankanj2000@gmail.com>
2023-01-05 22:10:48 +0300

    documented extended hello

--- 04_Challenges/NodeJS-Challenge/tasks.js
+++ 04_Challenges/NodeJS-Challenge/tasks.js
@@ -1,102 +1,100 @@
 
 /**
- * Starts the application
- * This is the function that is run when the app starts
- * 
- * It prints a welcome line, and then a line with "----",
- * then nothing.
- *  
- * @param  {string} name the name of the app
- * @returns {void}
- */
+* Starts the application
+* This is the function that is run when the app starts
+* 
+* It prints a welcome line, and then a line with "----",
+* then nothing.
+* 
+* @param {string} name the name of the app
+* @returns {void}
+*/
 function startApp(name){
   process.stdin.resume();
   process.stdin.setEncoding('utf8');
   process.stdin.on('data', onDataReceived);
   console.log(`Welcome to ${name}'s application!`)
   console.log("--------------------")
-}
-
-
-/**
- * Decides what to do depending on the data that was received
- * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
- * ```
- * node tasks.js batata
- * ```
- * 
- * The text received would be "batata"
- * This function  then directs to other functions
- * 
- * @param  {string} text data typed by the user
- * @returns {void}
- */
-function onDataReceived(text) {
-  if (text === 'quit\n') {
-    quit();
   }
   
+  /**
+  * Decides what to do depending on the data that was received
+  * This function receives the input sent by the user.
+  * 
+  * For example, if the user entered 
+  * ```
+  * node tasks.js batata
+  * ```
+  * 
+  * The text received would be "batata"
+  * This function then directs to other functions
+  * 
+  * @param {string} text data typed by the user
+  * @returns {void}
+  */
+  function onDataReceived(text) {
+  if (text === 'quit\n') {
+  quit();
+  }
   else if (text === 'exit\n' ) {
-    exit();
+  exit();
   }
-
+  else if (text.substring(0, 5) === "hello") {
+  hello(text.replace("\n", "").substring(6));
+  }
   else if(text === 'hello\n'){
-    hello();
+  hello();
   }
-
-   else if(text === 'help\n'){
-    help();
+  
+  else if(text === 'help\n'){
+  help();
   }
-
+  
   else{
-    unknownCommand(text);
+  unknownCommand(text);
   }
-}
-
-
-/**
- * prints "unknown command"
- * This function is supposed to run when all other commands have failed
- *
- * @param  {string} c the text received
- * @returns {void}
- */
-function unknownCommand(c){
+  }
+  
+  /**
+  * prints "unknown command"
+  * This function is supposed to run when all other commands have failed
+  *
+  * @param {string} c the text received
+  * @returns {void}
+  */
+  function unknownCommand(c){
   console.log('unknown command: "'+c.trim()+'"')
-}
-
-
-/**
- * Says hello
- *
- * @returns {void}
- */
-function hello(){
-  console.log('hello!')
-}
-
-
-/**
- * Exits the application
- *
- * @returns {void}
- */
-function quit(){
+  }
+  
+  /**
+  * Says hello
+  *
+  * @returns {void}
+  */
+  function hello(x){
+  console.log('hello!' + x + '!')
+  }
+  
+  /**
+  * Exits the application
+  *
+  * @returns {void}
+  */
+  function quit(){
   console.log('Quitting now, goodbye!')
   process.exit();
-}
-
-function exit(){
+  }
+  
+  function exit(){
   console.log('Exitting now, goodbye!')
   process.exit();
-}
-
-function help(){
+  }
+  
+  function help(){
   console.log('\nType `hello to greet our app \n' + 'Type `quit to exit our app \n' + 'Type `exit to exit our app \n')
-  process.exit();
-}
-
-// The following line starts the application
-startApp("Hasan Kanj")
+  }
+  
+  // The following line starts the application
+  startApp("Hasan Kanj")
+  
+  
