38bbc9e7f5930817f7f579b603c69e932692183a
hasan <hasankanj2000@gmail.com>
2023-01-07 14:49:08 +0300

    step 4

--- 04_Challenges/movie-database/index.js
+++ 04_Challenges/movie-database/index.js
@@ -13,6 +13,20 @@ app.get('/time', (req, res) => {
     const currentTime = new Date().toLocaleTimeString();
     res.json({ status: 200, message: currentTime });
   });
+
+  app.get('/hello/:id', (req, res) => {
+    const id = req.params.id || 'user';
+    res.json({ status: 200, message: `Hello, ${id}` });
+  });
+
+  app.get('/search', (req, res) => {
+    const search = req.query.s;
+    if (search) {
+      res.json({ status: 200, message: 'ok', data: search });
+    } else {
+      res.json({status: 500, error: true, message: 'you have to provide a search' });
+    }
+  });
   
 app.listen(3000, () => {
   console.log('Server listening on port 3000');
